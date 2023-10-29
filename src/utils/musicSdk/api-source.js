ab7bb321f9eed15061b3534be9f9af1ba021b72d
xiaojidong23 <130122263+xiaojidong23@users.noreply.github.com>
2023-10-29 16:28:19 +0800

    Update api-source.js

--- src/utils/musicSdk/api-source.js
+++ src/utils/musicSdk/api-source.js
@@ -1,53 +1,38 @@
 import apiSourceInfo from './api-source-info'
+import { apiSource, userApi } from '@renderer/store'
+import api_temp_kw from './kw/api-temp'
+// import api_test_bd from './bd/api-test'
+import api_test_tx from './tx/api-test'
+import api_test_kg from './kg/api-test'
+import api_test_kw from './kw/api-test'
+import api_test_mg from './mg/api-test'
+import api_test_wy from './wy/api-test'
 
-import temp_api_kw from './kw/api-temp'
-import test_api_kg from './kg/api-test'
-import test_api_kw from './kw/api-test'
-import test_api_tx from './tx/api-test'
-import test_api_wy from './wy/api-test'
-import test_api_mg from './mg/api-test'
-
-import direct_api_kg from './kg/api-direct'
-import direct_api_kw from './kw/api-direct'
-import direct_api_tx from './tx/api-direct'
-import direct_api_wy from './wy/api-direct'
-import direct_api_mg from './mg/api-direct'
-
-import settingState from '@/store/setting/state'
-
-
-const apiList = {
-  temp_api_kw,
-  // test_api_bd: require('./bd/api-test'),
-  test_api_kg,
-  test_api_kw,
-  test_api_tx,
-  test_api_wy,
-  test_api_mg,
-  direct_api_kg,
-  direct_api_kw,
-  direct_api_tx,
-  direct_api_wy,
-  direct_api_mg,
-  // test_api_tx: require('./tx/api-test'),
-  // test_api_wy: require('./wy/api-test'),
-  // test_api_xm: require('./xm/api-test'),
+const allApi = {
+  temp_kw: api_temp_kw,
+  // test_bd: api_test_bd,
+  test_tx: api_test_tx,
+  test_kg: api_test_kg,
+  test_kw: api_test_kw,
+  test_mg: api_test_mg,
+  test_wy: api_test_wy,
 }
+
+const apiList = {}
 const supportQuality = {}
 
 for (const api of apiSourceInfo) {
   supportQuality[api.id] = api.supportQualitys
-  // for (const source of Object.keys(api.supportQualitys)) {
-  //   const path = `./${source}/api-${api.id}`
-  //   console.log(path)
-  //   apiList[`${api.id}_api_${source}`] = path
-  // }
+  for (const source of Object.keys(api.supportQualitys)) {
+    apiList[`${api.id}_api_${source}`] = allApi[`${api.id}_${source}`]
+  }
 }
 
-const getAPI = source => apiList[`${settingState.setting['common.apiSource']}_api_${source}`]
+const getAPI = source => apiList[`${apiSource.value}_api_${source}`]
 
 const apis = source => {
-  const api = getAPI(source)
+  if (/^user_api/.test(apiSource.value)) return userApi.apis[source]
+  let api = getAPI(source)
   if (api) return api
   throw new Error('Api is not found')
 }
